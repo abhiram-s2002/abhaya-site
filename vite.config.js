0f3dcaf4780612819f0789d0fd32b572a667f16e
Your Name <you@example.com>
2026-09-13 14:07:09 +0530

    fix: set base path to root for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.BASE_PATH || (process.env.NODE_ENV === 'production' ? '/abhaya-site/' : '/'),
+  base: process.env.BASE_PATH || '/',
   plugins: [react()],
   server: {
     port: 3000,
