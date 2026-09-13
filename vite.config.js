4d82952ac8a9fd71c7447fa458ae7d51be08acde
Your Name <you@example.com>
2026-09-13 10:46:23 +0530

    d

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: process.env.BASE_PATH || (process.env.NODE_ENV === 'production' ? '/abhaya-site/' : '/'),
   plugins: [react()],
   server: {
     port: 3000,
